3f273218107cb0c4c258d338af1340b04697bc7c
Mykhailo Kovalskyi <m.kowalski.als@gmail.com>
2015-12-29 11:32:00 +0100

    Solutions to 5th chapter's exercises

--- src/main/scala/chapter05/Main.scala
+++ src/main/scala/chapter05/Main.scala
@@ -20,6 +20,10 @@ object Main extends App {
 
     // Create a schema
     exec(users.schema.create)
+    // Add some users
+    exec(
+      users ++= Seq(User("dave", Some("dave@example.org")), User("mike", None))
+    )
 
 
     // 5.3.7.1 Filtering Optional Columns
@@ -34,13 +38,36 @@ object Main extends App {
       case Some(x) => exec(users.filter(_.email === x).result)
       case None => exec(users.result)
     }
-
-    exec(
-      users ++= Seq(User("dave", Some("dave@example.org")), User("mike", None))
-    )
-
     require(2 == filterByEmail(None).length)
     require(1 == filterByEmail(Some("dave@example.org")).length)
+
+
+    // 5.3.7.2 Inside the Option
+    // Build on the last exercise to match rows that start with the supplied optional value.
+    // Recall that Rep[String] defines startsWith.
+    // So this time even filterByEmail(Some("dave@")).run will produce one row.
+    def filterByEmail2(email: Option[String]) = email match {
+      case Some(x) => exec(users.filter(_.email startsWith x).result)
+      case None => exec(users.result)
+    }
+    require(2 == filterByEmail2(None).length)
+    require(1 == filterByEmail2(Some("dave@example.org")).length)
+    require(1 == filterByEmail2(Some("dave@")).length)
+
+
+    // 5.3.7.3 Matching or Undecided
+    // Not everyone has an email address, so perhaps when filtering it would be safer to only exclude rows that don’t
+    //    match our filter criteria. That is, keep NULL addresses in the results.
+    // Add Elena to the database...
+    //    insert += User("Elena", Some("elena@example.org"))
+    // ...and modify filterByEmail so when we search for Some("elena@example.org") we only exclude Dave, as he definitely doesn’t match that address.
+    // This me you can do this in one query.
+    exec(users += User("Elena", Some("elena@example.org")))
+    def filterByEmail3(email: Option[String]) =
+      exec(users.filter(user => user.email.isEmpty || user.email === email).result)
+    require(1 == filterByEmail3(None).length)
+    require(2 == filterByEmail3(Some("elena@example.org")).length)
+
 
   } finally db.close()
 }
